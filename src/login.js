70c9c34fe250d9107748b31f9e7193837e74f07d
RitikaShrestha-me <ritika@baloo.chat>
2023-09-12 16:47:05 +0545

    email pwd verification in login page

--- src/login.js
+++ src/login.js
@@ -1,5 +1,27 @@
 let LoginButton = document.getElementById("js__login__button");
 
-LoginButton.addEventListener("click", function () {
-	window.location.href = '/home.html';
-});
+LoginButton.addEventListener("click", () => gotoHome());
+
+function gotoHome() {
+	let EmailInput = document.getElementById("js__id__email");
+	let PasswordInput = document.getElementById("js__id__password");
+	let InputErrMsg = document.getElementById("js__input__error");
+
+	// Comparing email regex
+	if (String(EmailInput.value).toLowerCase().match(
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	)) {
+		// Comparing Password Regex
+		if (String(PasswordInput.value).toLowerCase().match(
+			/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+		)) { window.location.href = '/home.html'; }
+		else {
+			InputErrMsg.textContent = "Invalid Password"
+		}
+	}
+	else {
+		InputErrMsg.textContent = "Invalid Email"
+	}
+
+
+}
